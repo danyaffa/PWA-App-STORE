a247e928451887c0b4f20525979a0fadf8b0080a
Dan Leffler <leffleryd@gmail.com>
2026-02-23 18:29:26 +1100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({ include: /\.(jsx|js)$/ }),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
